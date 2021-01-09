820f93d338424e62e9b71d824f38251c035b62b2
Hernan.Reiq <hernandemure1202@gmail.com>
2021-01-09 09:24:55 -0400

    Objetos de rutas y usuarios creados, mejorada la funcion de cambiarRuta

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var usuario = document.getElementById('usuario');
+var carro = document.getElementById('carro');
 var ruta = document.getElementById('ruta');
 var cambiar = document.getElementById('cambiar');
 
@@ -25,6 +27,14 @@ function cambiarRuta(){
             posicionRuta = 1;
             break;
     }
+    for(var i = 0; i <= 2; i++){
+        if (i == (posicionRuta - 1)){
+            rutas[posicionRuta - 1].activa = true;
+        } else {
+            rutas[i].activa = false;
+        }
+    }
+    persona["actualizarUbicacion"]();
 }
 
 /*
@@ -32,3 +42,48 @@ function cambiarRuta(){
 */
 cambiar.addEventListener('click', cambiarRuta);
 
+/*
+    RUTAS EXISTENTES
+*/
+var rutas = [
+    {
+        posicion_Y: [30, 30, 60, 85, 130, 180, 180, 200, 210, 250, 305, 315, 350, 363, 390, 420],
+        posicion_X: [105, 150, 170, 207, 207, 175, 225, 260, 265, 260, 285, 295, 305, 335, 360, 360],
+        activa: true
+    },
+    {
+        posicion_Y: [110, 160, 180, 210, 275, 355, 450],
+        posicion_X: [117, 117, 150, 175, 170, 220, 212],
+        activa: false
+    },
+    {
+        posicion_Y: [190, 165, 190, 185, 200, 203, 250, 270, 310, 345],
+        posicion_X: [370, 305, 230, 195, 170, 145, 80, 70, 30, 10],
+        activa: false
+    }
+];
+/*
+    OBJETO QUE TIENE LA UBICACION DEL USUARIO DEPENDIENDO DE LA RUTA
+*/
+var persona = {
+    ubicacion_Y: 180,
+    ubicacion_X: 175,
+    actualizarUbicacion: function (){
+        if(rutas[0].activa){
+            this.ubicacion_Y = 180;
+            this.ubicacion_X = 175;
+        } else if (rutas[1].activa){
+            this.ubicacion_Y = 210;
+            this.ubicacion_X = 175;
+        } else if (rutas[2].activa){
+            this.ubicacion_Y = 200;
+            this.ubicacion_X = 170;
+        }
+        usuario.style.top = this.ubicacion_Y + "px";
+        usuario.style.right = this.ubicacion_X + "px";
+    }
+}
+/*
+    ASIGNANDO LA UBICACION DEL USUARIO CUANDO INICIA LA APLICACION
+*/
+persona.actualizarUbicacion();
